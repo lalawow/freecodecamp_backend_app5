518919cc7a861c8edd33f21d6b3be3989a7f0501
Wei <lalawow@gmail.com>
2016-05-08 08:30:25 +0000

    FCC backend project5:Image Search Abstraction Layer

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,171 +1,104 @@
 'use strict';
-
+var https = require('https');
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 var mongo = require('mongodb').MongoClient
-var short_url_base = "https://freecodecamp-backend-app3-lalawow-1.c9users.io/"
+var short_url_base = "https://freecodecamp-backend-app5-lalawow.c9users.io/"
 var mongo_url = "mongodb://localhost:27017/clementinejs"
-var count = 0
-
-mongo.connect(mongo_url, function(err, db) {
-	if (err) throw err
-	var collection = db.collection('short_urls')
-	collection.find({
-		count: {
-			$exists: true
-		}
-	}).toArray(function(err, data_count) {
-		if (err) throw err
-		if (data_count.length !== 0) {
-			count = data_count[0].count
-			console.log("data count is " + count + " now!")
-			db.close()
-		}
-		else {
-
-			collection.insert({
-				dataname: "count",
-				"count": 0
-			}, function(err, data) {
-				if (err) throw err
-				db.close()
-			})
-		}
-	})
-})
-
-
-
 
 
 module.exports = function(app, passport) {
+	
+	//for freecodecamp back end porject: Image Search Abstraction Layer
 
-	function isLoggedIn(req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		}
-		else {
-			res.redirect('/login');
-		}
-	}
+	// https://cryptic-ridge-9197.herokuapp.com/api/imagesearch/lolcats%20funny?offset=10
+	app.route('/api/imagesearch/:query*').get(function(request, result) {
+		var searchTerm = request.params.query
+		var searchVol = request.query.offset
+		console.log(typeof(searchTerm) + ": " + searchTerm)
+		console.log(typeof(searchVol) + ": " + searchVol)
 
-	var clickHandler = new ClickHandler();
-
-
-
-	//for freecodecamp back end porject: URL Shortener Microservice
-	/*
-	User stories:
-
-	I can pass a URL as a parameter and I will receive a shortened URL in the JSON response.
-	When I visit that shortened URL, it will redirect me to my original link.
-	Example creation usage:
-	https://little-url.herokuapp.com/new/https://www.google.com
-	https://little-url.herokuapp.com/new/http://foo.com:80
-	Example creation output
-	{ "original_url":"http://foo.com:80", "short_url":"https://little-url.herokuapp.com/8170" }
-	Usage:
-	https://little-url.herokuapp.com/2871
-	Will redirect to:
-	https://www.google.com/
-	*/
-	app.route('/new/http://:_url').get(function(req, res) {
-		var url = req.params._url
-		console.log("address: http://" + url)
-		insertUrl("http://" + url, req, res)
-	})
-
-	app.route('/new/https://:_url').get(function(req, res) {
-		var url = req.params._url
-		console.log("address: https://" + url)
-		insertUrl("https://" + url, req, res)
-	})
-
-	app.route('/new/:_url').get(function(req, res) {
-		var url = req.params._url
-			res.writeHead(200, {
-		'Content-Type': 'application/json'
-	})
-	res.end("Sorry, it's an invalid url.")
-
-	})
+		var options = {
+			host: 'www.googleapis.com',
+			path: '/customsearch/v1?q=' + searchTerm.replace(/\s/g, "+") + '&cx=012332868486854605030:dwqv03pslfi&num=' + searchVol + '&searchType=image&start=1&key=AIzaSyDnI807mGZ1juoUt8smEzDm8ErDb7rQ1KM'
+		};
 
 
-	app.route('/:_id').get(function(req, res) {
-		var short_url_id = req.params._id
-		if (filterInt(short_url_id) > 0) {
-			var original_url = ""
+		var googleRequest = https.get(options, function(res) {
+			console.log('STATUS: ' + googleRequest.statusCode);
+			console.log('HEADERS: ' + JSON.stringify(googleRequest.headers));
 
-			mongo.connect(mongo_url, function(err, db) {
-				if (err) throw err
-				var collection = db.collection('short_urls')
-				collection.find({
-					short_url_id: {
-						$eq: short_url_id
+			// Buffer the body entirely for processing as a whole.
+			var bodyChunks = [];
+			res.on('data', function(chunk) {
+				// You can process streamed parts here...
+				bodyChunks.push(chunk);
+			}).on('end', function() {
+				var body = Buffer.concat(bodyChunks);
+				//console.log('BODY: ' + body);
+				// ...and/or process the entire body here.
+				var bodyJSON = JSON.parse(body)
+				var item = bodyJSON.items
+				console.log(item.length)
+				console.log(JSON.stringify(item[0]))
+				var searchAnswer = []
+				for (var i = 0; i < parseInt(searchVol); i++) {
+					//					{"url":"http://www.wallpapersxl.com/wallpapers/1440x900/flamenco/169257/flamenco-lolcats-funny-169257.jpg","snippet":"Lolcats Funny 1440x900","thumbnail":"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcStyjj01vszOWOaJvzp6bpgCeyeoKezI_AQ1jTZ-IiHkaQQIT-5WSnYgqg","context":"http://www.wallpapersxl.com/wallpaper/1440x900/flamenco-lolcats-funny-169257.html"}
+					var oneAnswer = {
+						"url": item[i].link,
+						"snippet": item[i].snippet,
+						"thumbnail": item[i].image.thumbnailLink,
+						"context": item[i].image.contextLink
 					}
-				}).toArray(function(err, urls) {
+					searchAnswer.push(oneAnswer)
+				}
+				result.send(JSON.stringify(searchAnswer))
+			})
+		});
+
+		googleRequest.on('error', function(e) {
+			console.log('ERROR: ' + e.message);
+		});
+
+		mongo.connect(mongo_url, function(err, db) {
+				if (err) throw err
+				var collection = db.collection('imageSearch')
+				var insert_info = {
+					term: searchTerm,
+					when: new Date()
+				}
+				console.log("insert to mongoDB: " + JSON.stringify(insert_info))
+				collection.insert(insert_info, function(err, data) {
 					if (err) throw err
-					original_url = urls[0]["original_url"]
-					console.log("we'll redirect to " + original_url)
-					res.redirect(original_url)
 					db.close()
 				})
 			})
-		}
-		else {
-			console.log("wrong route: " + short_url_id)
-		}
+			//		result.send("ok")
 
 	})
-	
-	app.route('/').get(function(req, res) {
-		res.sendFile(path + '/public/index.html');
-	})
 
-};
-
-var filterInt = function(value) {
-	if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value))
-		return Number(value);
-	return NaN;
-}
-
-var insertUrl = function(original_url, req, res) {
-	count++
-	var short_num = count
-	console.log("we got count " + short_num + " now!")
-	var short_url = short_url_base + short_num
-	var output = {
-		"original_url": original_url,
-		"short_url": short_url
-	}
-
-	var insert_info = {
-		"original_url": original_url,
-		"short_url_id": short_num.toString()
-	}
-
-	mongo.connect(mongo_url, function(err, db) {
-		if (err) throw err
-		var collection = db.collection('short_urls')
-		collection.insert(insert_info, function(err, data) {
+// https://cryptic-ridge-9197.herokuapp.com/api/latest/imagesearch/
+	app.route('/api/latest/imagesearch').get(function(req, res) {
+		mongo.connect(mongo_url, function(err, db) {
 			if (err) throw err
-			collection.update({
-				dataname: 'count'
-			}, {
-				$set: {
-					count: count
-				}
-			}, function(err) {
+			var collection = db.collection('imageSearch')
+			collection.find({}, {
+				term: 1,
+				when: 1,
+				_id: 0
+			}).sort({
+				$natural: -1
+			}).limit(10).toArray(function(err, searches) {
 				if (err) throw err
-				console.log("update count" + short_num + " on db")
+				res.send(JSON.stringify(searches))
 				db.close()
 			})
 		})
 	})
-	res.writeHead(200, {
-		'Content-Type': 'application/json'
+
+/*
+	app.route('/').get(function(req, res) {
+		res.sendFile(path + '/public/index.html');
 	})
-	res.end(JSON.stringify(output))
-}
+*/
+};
 
